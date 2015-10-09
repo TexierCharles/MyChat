0dc42a0ce07c3e3f19f8f9c247f3ad16b9448f28
Arthur Bochard <Arthur@MacBook-Pro-de-Arthur.local>
2015-10-09 10:52:27 +0200

    update
    ajout activité new message
    ajout activité  liste
    
    ajout bouton envoyer message + asynctask (pas encore fonctionnel)

--- app/src/main/java/com/arthurbochard/cctexier/mychatapplication/MenuActivity.java
+++ app/src/main/java/com/arthurbochard/cctexier/mychatapplication/MenuActivity.java
@@ -1,15 +1,25 @@
 package com.arthurbochard.cctexier.mychatapplication;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MenuActivity extends Activity {
 
+    Button button1;
+    Button button2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         // Retrieve views
@@ -23,5 +33,53 @@ public class MenuActivity extends Activity {
 
         // Set welcome text into text view
         welcomeText.setText(welcomeMsg);
+
+        addListenerOnButton1();
+        addListenerOnButton2();
+
+
+
+    }
+
+        public void addListenerOnButton1() {
+
+            final Context context = this;
+
+            button1 = (Button) findViewById(R.id.button1);
+
+            button1.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+
+                    Intent intent = new Intent(context, NewMessageActivity.class);
+                    startActivity(intent);
+
+                }
+
+            });
+
+        }
+
+
+
+    public void addListenerOnButton2() {
+
+        final Context context = this;
+
+        button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, ListActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
---
+++ app/src/main/java/com/arthurbochard/cctexier/mychatapplication/NewMessageActivity.java
@@ -0,0 +1,113 @@
+package com.arthurbochard.cctexier.mychatapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class NewMessageActivity extends Activity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_message);
+
+        addListenerOnButton();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_message, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
+    }
+
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                switch (arg0.getId()) {
+                    case R.id.button1:
+                        new SendMessage().execute("");
+                        break;
+                }
+
+                Intent intent = new Intent(context, MenuActivity.class);
+                startActivity(intent);
+
+            }
+
+
+        });
+
+    }
+
+
+    private class SendMessage extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.interrupted();
+                }
+            }
+            return "Executed";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView txt = (TextView) findViewById(R.id.output);
+            txt.setText("Executed"); // txt.setText(result);
+            // might want to change "executed" for the returned string passed
+            // into onPostExecute() but that is upto you
+        }
+
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
+    }
+
+
+
+
+
+
+}
